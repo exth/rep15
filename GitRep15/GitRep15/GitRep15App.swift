19eecb9b09489c96e9a04a5a84d5f00b852e5d47
exth <exth331@gmail.com>
2026-04-17 05:14:18 +0300

    Add Xcode file & Add gitignore

---
+++ GitRep15/GitRep15/GitRep15App.swift
@@ -0,0 +1,17 @@
+//
+//  GitRep15App.swift
+//  GitRep15
+//
+//  Created by Вадим on 17.04.26.
+//
+
+import SwiftUI
+
+@main
+struct GitRep15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
